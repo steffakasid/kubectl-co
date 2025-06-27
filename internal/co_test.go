b1b041594bd54a55fdb693d2942d5594e4e3be62
steffakasid <github@steffen-rumpf.de>
2025-06-27 22:23:07 +0200

    refactor(tests): simplify initCO function and update related tests

--- internal/co_test.go
+++ internal/co_test.go
@@ -9,13 +9,13 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func initCO(t *testing.T) (co *CO, home string, kubeHome string, coHome string, previousFile string, previousLink string) {
-	home = t.TempDir()
+func initCO(t *testing.T) (co *CO) {
+	home := t.TempDir()
 
-	kubeHome = path.Join(home, ".kube")
-	coHome = path.Join(kubeHome, "co")
-	previousFile = path.Join(coHome, "previousconfig")
-	previousLink = path.Join(coHome, "previous")
+	kubeHome := path.Join(home, ".kube")
+	coHome := path.Join(kubeHome, "co")
+	previousFile := path.Join(coHome, "previousconfig")
+	previousLink := path.Join(coHome, "previous")
 
 	err := os.Mkdir(kubeHome, 0777)
 	require.NoError(t, err)
@@ -31,7 +31,7 @@ func initCO(t *testing.T) (co *CO, home string, kubeHome string, coHome string, 
 
 	co, err = NewCO(home)
 	require.NoError(t, err)
-	return co, home, kubeHome, coHome, previousFile, previousLink
+	return co
 }
 
 func TestNewCO(t *testing.T) {
@@ -43,43 +43,89 @@ func TestNewCO(t *testing.T) {
 	})
 }
 
+func TestInitKubeHome(t *testing.T) {
+	t.Run("Success", func(t *testing.T) {
+		home := t.TempDir()
+		kubeHome := path.Join(home, ".kube")
+		err := initKubeHome(kubeHome)
+		require.NoError(t, err)
+	})
+}
+
+func TestInitCOHome(t *testing.T) {
+	t.Run("Success", func(t *testing.T) {
+		co := CO{
+			CObasePath: path.Join(t.TempDir(), "co"),
+		}
+		err := co.initCOHome()
+		require.NoError(t, err)
+		assert.DirExists(t, co.CObasePath)
+	})
+	t.Run("Parent does not exist", func(t *testing.T) {
+		co := CO{
+			CObasePath: path.Join(t.TempDir(), "parent", "co"),
+		}
+		err := co.initCOHome()
+		require.Error(t, err)
+		require.Contains(t, err.Error(), "no such file or directory")
+		assert.NoDirExists(t, co.CObasePath)
+	})
+}
+
 func TestAddConfig(t *testing.T) {
 
 	t.Run("Add new config", func(t *testing.T) {
-		co, _, _, coHome, _, _ := initCO(t)
+		co := initCO(t)
 		co.ConfigName = "testconfig"
-		expectedConfigFile := path.Join(coHome, "testconfig")
+		expectedConfigFile := path.Join(co.CObasePath, "testconfig")
 		err := co.AddConfig("")
 		require.NoError(t, err)
 		assert.FileExists(t, expectedConfigFile)
 	})
 
-	t.Run("Add new config", func(t *testing.T) {
-		co, _, _, coHome, _, _ := initCO(t)
+	t.Run("Add new config 2", func(t *testing.T) {
+		co := initCO(t)
 		co.ConfigName = "testconfig"
-		expectedConfigFile := path.Join(coHome, "testconfig")
+		expectedConfigFile := path.Join(co.CObasePath, "testconfig")
 		err := co.AddConfig("../test/test.yml")
 		require.NoError(t, err)
 		assert.FileExists(t, expectedConfigFile)
+	})
+
+	t.Run("Add not existing config", func(t *testing.T) {
+		co := initCO(t)
+		co.ConfigName = "testconfig"
+		err := co.AddConfig("not-existing.yml")
+		require.Error(t, err)
+		require.Contains(t, err.Error(), "no such file or directory")
+	})
+
+	t.Run("Add missconfigured co name", func(t *testing.T) {
+		co := initCO(t)
+		co.ConfigName = "testconfig"
+		co.CObasePath = "../no-existing-path"
+		err := co.AddConfig("../test/test.yml")
+		require.Error(t, err)
+		require.Contains(t, err.Error(), "no such file or directory")
 	})
 }
 
 func TestLinkKubeConfig(t *testing.T) {
 	t.Run("Success", func(t *testing.T) {
-		co, _, kubeHome, coHome, _, _ := initCO(t)
+		co := initCO(t)
 
 		co.ConfigName = "testconfig"
-		configFile := path.Join(coHome, co.ConfigName)
-		expectedFile := path.Join(kubeHome, "config")
+		configFile := path.Join(co.CObasePath, co.ConfigName)
+
 		_, err := os.Create(configFile)
 		require.NoError(t, err)
 
 		err = co.LinkKubeConfig()
 
 		require.NoError(t, err)
-		assert.FileExists(t, expectedFile)
+		assert.FileExists(t, co.KubeConfigPath)
 
-		expectedLink, err := os.Readlink(expectedFile)
+		expectedLink, err := os.Readlink(co.KubeConfigPath)
 		require.NoError(t, err)
 		assert.Equal(t, configFile, expectedLink)
 	})
@@ -87,23 +133,22 @@ func TestLinkKubeConfig(t *testing.T) {
 
 func TestCleanup(t *testing.T) {
 	t.Run("Cleanup only kube config", func(t *testing.T) {
-		co, _, kubeHome, _, _, _ := initCO(t)
+		co := initCO(t)
 
-		kubeConfig := path.Join(kubeHome, "config")
-		_, err := os.Create(kubeConfig)
+		_, err := os.Create(co.KubeConfigPath)
 		require.NoError(t, err)
 
 		err = co.cleanup()
 		require.NoError(t, err)
-		assert.NoFileExists(t, kubeConfig)
+		assert.NoFileExists(t, co.KubeConfigPath)
 	})
 }
 
 func TestDeleteConfig(t *testing.T) {
 	t.Run("Success", func(t *testing.T) {
-		co, _, _, coHome, _, _ := initCO(t)
+		co := initCO(t)
 		co.ConfigName = "testconfig"
-		configFile := path.Join(coHome, co.ConfigName)
+		configFile := path.Join(co.CObasePath, co.ConfigName)
 
 		_, err := os.Create(configFile)
 		require.NoError(t, err)
@@ -115,15 +160,16 @@ func TestDeleteConfig(t *testing.T) {
 }
 
 func TestListConfigs(t *testing.T) {
+
 	t.Run("Success", func(t *testing.T) {
-		co, _, _, coHome, _, _ := initCO(t)
+		co := initCO(t)
 		co.ConfigName = "testconfig"
 		anotherConfig := "anotherone"
-		configFile := path.Join(coHome, co.ConfigName)
+		configFile := path.Join(co.CObasePath, co.ConfigName)
 		_, err := os.Create(configFile)
 		require.NoError(t, err)
 
-		anotherFile := path.Join(coHome, anotherConfig)
+		anotherFile := path.Join(co.CObasePath, anotherConfig)
 		_, err = os.Create(anotherFile)
 		require.NoError(t, err)
 
@@ -132,4 +178,13 @@ func TestListConfigs(t *testing.T) {
 		assert.Equal(t, []string{anotherConfig, "previousconfig", co.ConfigName}, configs)
 	})
 
+	t.Run("Error", func(t *testing.T) {
+		co := initCO(t)
+		co.CObasePath = "not-existing-path"
+
+		configs, err := co.ListConfigs()
+		require.Error(t, err)
+		require.Len(t, configs, 0)
+		require.Contains(t, err.Error(), "no such file or directory")
+	})
 }
